ea62349bb8e03cdf48094cf18c2c001500daf7ed
Yusuf Çokal <134581731+yusuffcx@users.noreply.github.com>
2024-12-04 21:17:54 +0300

    added take picture for favorite places

--- lib/widget/image_input.dart
+++ lib/widget/image_input.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ImageInput extends StatefulWidget {
   const ImageInput({super.key});
@@ -10,22 +13,38 @@ class ImageInput extends StatefulWidget {
 }
 
 class _ImageInputState extends State<ImageInput> {
-  void _takePicture() {}
+  var img;
+  void _takePicture() async {
+    final takenImage = await ImagePicker()
+        .pickImage(source: ImageSource.camera, maxWidth: 600);
+    setState(() {
+      if (takenImage != null) {
+        img = File(takenImage.path);
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    Widget body = img != null
+        ? GestureDetector(
+            onTap: _takePicture,
+            child: Image.file(img, fit: BoxFit.cover),
+          )
+        : ElevatedButton.icon(
+            onPressed: _takePicture,
+            label: const Text('Take Picture'),
+            icon: const Icon(Icons.camera));
+
     return Container(
-      width: double.infinity,
-      alignment: Alignment.center,
-      height: 250,
-      decoration: BoxDecoration(
-          border: Border.all(
-              width: 1,
-              color: Theme.of(context).colorScheme.onPrimary.withOpacity(0.2))),
-      child: ElevatedButton.icon(
-          onPressed: _takePicture,
-          label: const Text('Take Picture'),
-          icon: const Icon(Icons.camera)),
-    );
+        width: double.infinity,
+        alignment: Alignment.center,
+        height: 250,
+        decoration: BoxDecoration(
+            border: Border.all(
+                width: 1,
+                color:
+                    Theme.of(context).colorScheme.onPrimary.withOpacity(0.2))),
+        child: body);
   }
 }
