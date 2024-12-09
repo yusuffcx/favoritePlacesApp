93647ea35c2c930a07ee071f4e183b1bbcc737fc
Yusuf Çokal <134581731+yusuffcx@users.noreply.github.com>
2024-12-09 22:55:18 +0300

    updated code with google maps integration

--- lib/widget/location_input.dart
+++ lib/widget/location_input.dart
@@ -14,9 +14,10 @@ class LocationInput extends StatefulWidget {
 }
 
 class _LocationInputState extends State<LocationInput> {
-  PlaceLocation? loc;
+  Widget? map;
+  var gettingLoc = false;
+  var showMap = false;
 
-  var waitingGetLoc = false;
   void getCurrentLocation() async {
     Location location = Location();
 
@@ -39,56 +40,72 @@ class _LocationInputState extends State<LocationInput> {
         return;
       }
     }
+
     setState(() {
-      waitingGetLoc = true;
+      gettingLoc = true;
+      showMap = true;
     });
 
     locationData = await location.getLocation();
-    final lng = locationData.longitude;
     final lat = locationData.latitude;
-    if (lng == null || lat == null) {
-      return;
-    }
+    final lng = locationData.longitude;
 
-    final url = Uri.parse(
+    print(locationData.latitude);
+    print(locationData.longitude);
+    var url = Uri.parse(
         'https://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$lng&key=AIzaSyAzpUXdswADNpnJ_8iUhaJViA9KNH2R6as');
-    final resp = await http.get(url);
-    final data = json.decode(resp.body);
-    final address = data['results'][0]['formatted_address'];
+    var resp = await http.get(url);
+    print(resp);
+    print(resp.body);
+
+    var data = json.decode(resp.body);
+
+    var address = data["results"][0];
     print(address);
 
+    map = Image.network(
+        'https://maps.googleapis.com/maps/api/staticmap?center=$lat,$lng&zoom=16&size=400x400&markers=color:red%7Clabel:G%7C$lat,$lng&maptype=roadmap&key=AIzaSyAzpUXdswADNpnJ_8iUhaJViA9KNH2R6as',
+        fit: BoxFit.cover,
+        width: double.infinity,
+        height: double.infinity);
+
     setState(() {
-      loc = PlaceLocation(longitude: lng, latitude: lat, address: address);
-      waitingGetLoc = false;
+      gettingLoc = false;
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    Widget? content;
+    if (!gettingLoc) {
+      content = !showMap
+          ? Text('No location selected',
+              style: Theme.of(context)
+                  .textTheme
+                  .bodyLarge!
+                  .copyWith(color: Theme.of(context).colorScheme.onSurface),
+              textAlign: TextAlign.center)
+          : map;
+    } else {
+      content = const SizedBox(
+        width: 15,
+        height: 15,
+        child: CircularProgressIndicator(),
+      );
+    }
+
     return Column(children: [
       Container(
-        alignment: Alignment.center,
-        width: double.infinity,
-        height: 170,
-        decoration: BoxDecoration(
-            border: Border.all(
-          color: Theme.of(context).colorScheme.onPrimary.withOpacity(0.2),
-          width: 1,
-        )),
-        child: !waitingGetLoc
-            ? Text('No location selected',
-                style: Theme.of(context)
-                    .textTheme
-                    .bodyLarge!
-                    .copyWith(color: Theme.of(context).colorScheme.onSurface),
-                textAlign: TextAlign.center)
-            : const SizedBox(
-                width: 15,
-                height: 15,
-                child: CircularProgressIndicator(),
-              ),
-      ),
-      !waitingGetLoc
+          alignment: Alignment.center,
+          width: double.infinity,
+          height: 170,
+          decoration: BoxDecoration(
+              border: Border.all(
+            color: Theme.of(context).colorScheme.onPrimary.withOpacity(0.2),
+            width: 1,
+          )),
+          child: content),
+      !gettingLoc
           ? Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
